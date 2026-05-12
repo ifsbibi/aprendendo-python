5eb1710fcbda02ffdd298e5b7388257f485dfc96
ifsbibi <evilyngabrielly05@gmail.com>
2026-05-12 17:40:14 -0300

    codigos

---
+++ exercício01.py
@@ -0,0 +1,27 @@
+n1 = int(input("digte o primeiro número: "))
+n2 = int(input("digte o segundo número: "))
+
+resultado = n1 // n2
+resultado2 = n1 % n2
+resultado3 = n1 ** n2
+
+print("o resultado é ",resultado)
+print("o resultado2 é ",resultado2)
+print("o resultado3 é ",resultado3)
+
+
+
+
+print("--------------------------------------------------------------------------")
+print("   OPERADORES RELACIONAIS"      )
+print("--------------------------------------------------------------------------")
+
+
+relacao1 = n1 > n2
+relacao2 = n1 < n2
+relacao3 = n1 >= n2
+relacao4 = n1 <= n2
+relacao5 = n1 == n2
+relacao6 = n1 != n2
+
+print("Os resultados das relacao estarao abaixo: \n{} \n{} \n{} \n{} \n{} \n{}".format(relacao1, relacao2, relacao3, relacao4, relacao5, relacao6))
---
+++ progama01.py
@@ -0,0 +1,16 @@
+print("Hello Wordl!")
+print("one piece, é o melhor")
+print("1") # string (string de caracteres)
+print(1)    # int (número inteiro)
+print(-2)    # int (número inteiro)
+print(8.2)    # float (número real ou decimal, ponto flutuante)
+print(True)    # Verdadeiro (booleano - bool)
+print(False)    # Falso (booleano - bool)
+
+nome = "Maria" 
+idade = 19
+peso = 62.5
+maiorIdade = True
+
+
+print("Essa é a " ,nome, ", ela tem " ,idade," anos e pesa " ,peso," ")
