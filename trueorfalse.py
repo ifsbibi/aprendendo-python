65191920abca1a39bdac6c009f6cac4a62a92a27
ifsbibi <evilyngabrielly05@gmail.com>
2026-05-20 17:38:01 -0300

    Add files via upload

---
+++ trueorfalse.py
@@ -0,0 +1,28 @@
+print((15 > 7) and (3 == 3))
+
+print((10 < 4) or (8 != 5))
+
+print(not(20 >= 20))
+
+print((5 * 2 == 10) and not (3 < 4))
+
+print((12/12 == 5) or (7 <= 7))
+
+print(not (9 != 9) and (4>1))
+
+print((100 == 100) and ((5 > 10) or (2 < 3)))
+
+print(not(18 < 20) and (5 != 5))
+
+print((0 == 1) or not ((3 >=2) and (4 <=4)))
+
+print(((10 > 5) and not (2 == 3)) or (8 < 4))
+
+
+
+
+
+
+
+
+
